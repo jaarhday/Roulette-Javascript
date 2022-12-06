f8050817f39ccb03704f4e42af0b2343a8db6533
jaarhday <jaarhday@byui.edu>
2022-12-05 17:56:45 -0700

    notes added

--- scripts/roulettegame.js
+++ scripts/roulettegame.js
@@ -5,7 +5,7 @@ const message = createEle(gamearea,'div','Press Spin','message');
 const output = createEle(gamearea,'div','','output');
 const game = {x:7,y:9,coins:50,sel:[],eles:[],winner:false,styler:['black','white']};
 const total = game.x * game.y;
-btn.disabled = true;
+btn.disabled = true; //can't play until bet is placed
 btn.addEventListener('click',spinner);
 
 createBoard();
@@ -15,33 +15,33 @@ function spinner(){
     btn.disabled = true;
     const ranVal = Math.floor(Math.random()*total)+1;
     console.log(ranVal);
-    game.winner = ranVal-1;
+    game.winner = ranVal-1; //winner is set and styles box to a specific color
     game.styler = [game.eles[ranVal-1].style.backgroundColor,game.eles[ranVal-1].style.color];
-    const win = game.sel.includes(ranVal);
+    const win = game.sel.includes(ranVal); //
     console.log(win);
 
     const eles = output.querySelectorAll('.bet');
-    eles.forEach((el)=>{
+    eles.forEach((el)=>{ //removes the previous bets from the board
         el.remove();
         console.log(el);
     })
     if(win){
         const winAmount = total;
-        game.coins += winAmount;
-        message.innerHTML = `Winner on ${ranVal} you won ${winAmount}`;
+        game.coins += winAmount; 
+        message.innerHTML = `Winner on ${ranVal} you won ${winAmount}`; //prints out winner and value of winnings
         createEle(game.eles[ranVal-1],'div','$','bet');
         game.eles[ranVal-1].style.backgroundColor ='green';
     }
     else{
-        message.innerHTML = `Lost sorry you did not bet on ${ranVal}`; 
+        message.innerHTML = `Lost sorry you did not bet on ${ranVal}`; //player lost
         game.eles[ranVal-1].style.backgroundColor ='purple';
     }
-    game.sel = [];
-    updateScore();
+    game.sel = []; //resets the game selection array
+    updateScore(); //updates the score +- winnings/loses
 
     game.eles.forEach((el)=>{
         el.bet = false;
-    })
+    }) //resets game elements to false
 
 }
 
@@ -58,15 +58,15 @@ function createBoard(){
         game.eles.push(temp);
         temp.bet = false;
         temp.addEventListener('click',(e)=>{
-            btn.disabled = false;
-            if(game.winner){
+            btn.disabled = false; //allows to play again after making bet
+            if(game.winner){ //game winner index value
                 const parTemp = game.eles[game.winner];
                 parTemp.style.backgroundColor = game.styler[0];
-                parTemp.style.color = game.styler[1];  
+                parTemp.style.color = game.styler[1];  //used to store the color to reset background colors
                 game.winner = false;
                 const bets = parTemp.querySelector('.bet');
                 if(bets){
-                    bets.remove();
+                    bets.remove(); //resets bets
                 }
             }
             console.log(temp.textContent);
@@ -76,7 +76,7 @@ function createBoard(){
                 bets.remove();
                 //console.log(bets);
                 temp.bet = false;
-                game.coins++;
+                game.coins++; //adds coins to the game
                 const index = game.sel.indexOf(i+1);
                 if(index > -1){
                     game.sel.splice(index,1);
